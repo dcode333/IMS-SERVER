c3353cd3b438e90af52f8ca5bbb6fb834a3cca7b
dcode333 <98689944+dcode333@users.noreply.github.com>
2023-11-15 15:57:54 +0500

    ADDED course edit route in admin/courses

--- routes/admin/courses.js
+++ routes/admin/courses.js
@@ -3,23 +3,23 @@ const route = express.Router();
 const { check, validationResult } = require('express-validator');
 const Course = require('../../schemas/Course');
 
+const validateCourse = [
+    check('courseCode').isString().not().isEmpty(),
+    check('name').isString().not().isEmpty(),
+    check('department').not().isEmpty(),
+    check('strength').isInt({ min: 0 }).not().isEmpty(),
+    check('duration').isNumeric().isIn([7, 14, 30]),
+    check('author').isString().not().isEmpty(),
+    check('category').isString().not().isEmpty(),
+    check('description').isString().not().isEmpty(),
+]
+
 route.post(
-    '/course',
-    [
-        check('courseCode').isString().not().isEmpty(),
-        check('name').isString().not().isEmpty(),
-        check('department').not().isEmpty(),
-        check('strength').isInt({ min: 0 }).not().isEmpty(),
-        check('duration').isNumeric().isIn([7, 14, 30]),
-        check('author').isString().not().isEmpty(),
-        check('category').isString().not().isEmpty(),
-        check('description').isString().not().isEmpty(),
-    
-    ],
+    '/course', validateCourse,
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ success: false, errors: errors.array() });
+            return res.status(400).json({ success: false, error: errors.array() });
         }
 
         const {
@@ -49,10 +49,43 @@ route.post(
             res.status(201).json({ success: true, course: newAddedCourse });
         } catch (err) {
             console.error(err);
-            res.status(500).json({ success: false, error: 'Internal Server Error' });
+            res.status(500).json({ success: false, error: err.message });
         }
     }
 );
+
+route.put('/course/:id', validateCourse, async (req, res) => {
+
+    const courseId = req.params.id;
+    const {
+        courseCode,
+        name,
+        department,
+        strength,
+        duration,
+        author,
+        category,
+        description,
+    } = req.body;
+
+    try {
+        const course = await Course.findByIdAndUpdate(courseId, {
+            courseCode,
+            name,
+            department,
+            strength,
+            duration,
+            author,
+            category,
+            description,
+        }, { new: true });
+
+        res.status(200).json({ success: true, message: 'Course updated', data: course });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: true, error: err.message });
+    }
+});
 
 route.get('/courses', async (req, res) => {
     try {
